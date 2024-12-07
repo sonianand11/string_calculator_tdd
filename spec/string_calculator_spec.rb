7396b9e4217273dedac2b4fc804c7b3debf6cac0
Anand Soni <sonianand11@gmail.com>
2024-12-07 11:00:11 +0530

    Added new case to return number when given single number argument

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe StringCalculator do
     it "return 0 for an empty string" do
       expect(calculator.add("")).to eq(0)
     end
+    
+    it "returns the number itself for a single number" do
+      expect(calculator.add("1")).to eq(1)
+    end
   end
 
 end
