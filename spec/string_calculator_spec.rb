85dc15f46b9f681d471b5e7f66ea2d0564ecee08
Anand Soni <sonianand11@gmail.com>
2024-12-07 10:57:17 +0530

    Making test case pass by adding string_calculator class

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,8 +1,12 @@
+require_relative "../string_calculator"
+
 RSpec.describe StringCalculator do
   let(:calculator) {StringCalculator.new}
+
   describe "#add" do
-    it "return 0 for an emplty string" do
+    it "return 0 for an empty string" do
       expect(calculator.add("")).to eq(0)
     end
   end
+
 end
---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+class StringCalculator
+  
+  def add(string_numbers)
+    return 0 if string_numbers == ""
+  end
+
+end
